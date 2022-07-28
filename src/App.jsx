7069f12be7c5edd5d3779569a7ca3d467216141b
prakash-2021 <prakashrestha2075@gmail.com>
2022-07-28 18:21:54 +0545

    imported

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Meme } from "./components/Meme";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  );
+};
